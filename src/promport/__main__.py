bde505c126a220204614866d3b9fda53ee512463
kkorolyov <kirillk753@gmail.com>
2024-10-05 03:31:42 -0700

    beef up server a bit

--- src/promport/__main__.py
+++ src/promport/__main__.py
@@ -1,43 +1,78 @@
+import os
 import subprocess
+from argparse import ArgumentParser, RawTextHelpFormatter
+from functools import partial
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from os import environ
-from typing import BinaryIO
+from urllib.parse import parse_qs
 
-data = environ["DATA"]
+import requests
+
+
+def _parseArgs():
+    parser = ArgumentParser(
+        prog="promport",
+        description="HTTP server exposing endpoints to bulk-upload Prometheus data",
+        formatter_class=RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "--url",
+        type=str,
+        help="Prometheus API URL to proxy",
+        default="http://localhost:9090",
+    )
+    parser.add_argument("--data", type=str, help="TSDB data directory", required=True)
+    parser.add_argument(
+        "--maxBlockDuration",
+        type=str,
+        help="maximum block duration value to forward to promtool",
+        default="87600h",
+    )
+
+    return parser.parse_args()
 
 
 class Handler(BaseHTTPRequestHandler):
+    _url: str
+    _data: str
+    _maxBlockDuration: str
+
+    def __init__(
+        self, url: str, data: str, maxBlockDuration: str, *args, **kwargs
+    ) -> None:
+        self._url = url
+        self._data = data
+        self._maxBlockDuration = maxBlockDuration
+
+        super().__init__(*args, **kwargs)
+
     def do_POST(self):
-        print(f"handling request to {self.path} from {self.client_address}")
-
-        match self.path:
-            case "/import":
-                self._handleImport()
-            case _:
-                raise RuntimeError(f"unknown path: {self.path}")
-
-    def _handleImport(self):
         try:
-            importFile = self._writeFile(self.rfile)
-            self._tsdbImport(importFile)
+            match self.path:
+                case "/import":
+                    self._handleImport()
+                case "/delete":
+                    self._handleDelete()
+                case _:
+                    self.send_error(404)
 
             self.send_response(200)
             self.end_headers()
-        except RuntimeError as e:
-            print(f"error handling request: {e}")
-            self.send_response(500, str(e))
+        except Exception as e:
+            self.send_error(500)
+            raise e
 
-    def _writeFile(self, b: BinaryIO):
-        importFile = f"{data}/import.om"
+    def _handleImport(self):
+        length = self.headers["Content-Length"]
+        if not length:
+            self.send_error(400, "Missing Content-Length header")
+            return
+
+        self.log_message(f"importing data of length {length}...")
+
+        importFile = f"{self._data}/import.om"
         with open(importFile, "wb") as f:
-            print(f"writing request data to {importFile}...")
-            f.writelines(b)
-            print(f"done writing")
+            f.write(self.rfile.read(int(length)))
 
-        return importFile
-
-    def _tsdbImport(self, path: str):
-        print(f"importing TSDB data at {path} to {data}...")
         subprocess.check_call(
             [
                 "promtool",
@@ -45,26 +80,39 @@ class Handler(BaseHTTPRequestHandler):
                 "create-blocks-from",
                 "openmetrics",
                 "--max-block-duration",
-                "87600",
-                path,
-                data,
+                self._maxBlockDuration,
+                importFile,
+                self._data,
             ]
         )
-        print("done importing")
 
+        os.remove(importFile)
 
-def checkArgs():
-    if not data:
-        print("missing DATA env var")
-        exit(1)
+    def _handleDelete(self):
+        length = self.headers["Content-Length"]
+        if not length:
+            self.send_error(400, "Missing Content-Length header")
+            return
+
+        requests.post(
+            f"{self._url}/api/v1/admin/tsdb/delete_series",
+            parse_qs(self.rfile.read(int(length)).decode()),
+        ).raise_for_status()
+        requests.post(
+            f"{self._url}/api/v1/admin/tsdb/clean_tombstones"
+        ).raise_for_status()
 
 
 def main():
+    args = _parseArgs()
+
     address = "0.0.0.0"
     port = 9191
 
-    httpd = HTTPServer((address, port), Handler)
-    print(f"started server at {address}:{port}, with config (data={data})")
+    httpd = HTTPServer(
+        (address, port), partial(Handler, args.url, args.data, args.maxBlockDuration)
+    )
+    print(f"started server at {address}:{port}, with args {args}")
     httpd.serve_forever()
 
 
